d2705209410f196b79402ef16db21b24af1add5f
Vicky Kumar <imvickykumar999@gmail.com>
2024-03-25 19:07:37 +0530

    dynamic data pass

--- wscubetech/wscubetech/views.py
+++ wscubetech/wscubetech/views.py
@@ -6,7 +6,11 @@ def home(request):
     return render(request, 'index.html')
 
 def course(request):
-    return HttpResponse('Let, Course ID = <a href="/course-detail/100">100</a>')
+    data = {
+        'title' : 'Course Details',
+        'c_id' : 100
+    }
+    return render(request, 'course.html', data)
 
 def course_detail(request, course_id):
-    return HttpResponse(f'You have selected Course ID = {course_id}')
+    return HttpResponse(f'<h1>You have selected Course ID = {course_id}</h1> <br> <a href="/"><h2>Home</h2></a>')
